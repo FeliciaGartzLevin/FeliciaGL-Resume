fa8fa3a3156cf2e8a87981cdf6a5bbb15f42ff62
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-09-12 00:09:35 +0200

    setup of browserrouter, pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './assets/scss/App.scss'
-
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Navigation from './assets/scss/pages/partials/Navigation'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './assets/scss/pages/Homepage'
+import NotFoundPage from './assets/scss/pages/NotFoundPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div id='App'>
+      <Navigation />
+      <Container>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </Container>
+    </div>
   )
 }
 
